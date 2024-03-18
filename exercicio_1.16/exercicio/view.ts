8c04fb88f437d5e74479e904382fc403500fa2f7
IsaacSs32 <143840939+IsaacSs32@users.noreply.github.com>
2024-03-18 14:25:59 -0300

    Delete exercicio_1.16/exercicio/view.ts

--- exercicio_1.16/exercicio/view.ts
+++
@@ -1,9 +0,0 @@
-import exibirOpcaoSelecionada from "./exercicio1.16";
-
-
-console.log("Opção 1: Criar cadastro.")
-console.log("Opção 2: Excluir cadastro.")
-console.log("Opção 3: Atualizar cadastro.")
-console.log("Opção 4: listar cadastro.")
-
-exibirOpcaoSelecionada(0)
